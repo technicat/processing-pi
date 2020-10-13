581d2e8fca5350c72670878cd52af3bf752066ee
Philip Chu <atlassian@technicat.com>
2020-10-13 10:50:10 -0700

    more pi

---
+++ pi_polygons/publish.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+butler push application.osx technicat/processing-pi:osx
+butler push application.windows64 technicat/processing-pi:win
+butler push application.linux64 technicat/processing-pi:linux
