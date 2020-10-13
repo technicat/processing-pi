e89b21665f18e9b27aa588b4764c907eb57f6f81
Philip Chu <atlassian@technicat.com>
2020-10-13 10:38:55 -0700

    darts

---
+++ pi_darts/publish.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+butler push application.osx technicat/processing-pi:osx
+butler push application.win64 technicat/processing-pi:win
+butler push application.linux64 technicat/processing-pi:linux
