5c31455dd4dd412cfe004692d853c91b82421e2e
Philip Chu <atlassian@technicat.com>
2020-12-02 03:35:21 -0800

    cleanup for public

--- pi_polygons/publish.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-butler push application.osx technicat/processing-pi:osx
-butler push application.windows64 technicat/processing-pi:win
-butler push application.linux64 technicat/processing-pi:linux
